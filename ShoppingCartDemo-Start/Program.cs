9cc59057ce8f6df0e68fd8786002d1a9331d11c9
culi Nguyen <culi.nguyen@bellevuecollege.edu>
2017-11-09 20:39:50 -0800

    changed todo2

--- ShoppingCartDemo-Start/Program.cs
+++ ShoppingCartDemo-Start/Program.cs
@@ -215,10 +215,15 @@ namespace ShoppingCartDemo
             Console.WriteLine("Please choose from the following menu:");
 
             //TODO2: Put this in a loop
-            Console.WriteLine("1. {0}", _inventoryList[0].DisplayName());
-            Console.WriteLine("2. {0}", _inventoryList[2].DisplayName());
-            Console.WriteLine("3. {0}", _inventoryList[0].DisplayName());
-            Console.WriteLine("4. Finish purchasing");
+            for (int i = 0; i < listSize; i++)
+            {
+                Console.WriteLine("{0}. {1}", _inventoryList[i].DisplayName());
+            }
+            Console.WriteLine("{0}. Finish purchasing", listSize + 1);
+            //Console.WriteLine("1. {0}", _inventoryList[0].DisplayName());
+           // Console.WriteLine("2. {0}", _inventoryList[1].DisplayName());
+            //Console.WriteLine("3. {0}", _inventoryList[2].DisplayName());
+            //Console.WriteLine("4. Finish purchasing");
 
             Console.WriteLine("Which one would you like to purchase?");
 
